aee85e35503187ddc2eaede2fcfeacf03e0ce09c
Anusha-Devadiga79 <anushadevadiga79@gmail.com>
2025-07-29 20:18:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from 'url';
 
-plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-      "@shared": path.resolve(__dirname, "./shared"),
       "@": path.resolve(process.cwd(), "./client/src"),
       "@shared": path.resolve(process.cwd(), "./shared"),
     },
   },
   root: "./client",
--8
-+0
+  build: {
+    outDir: "../dist/public",
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      input: "./client/index.html",
+    },
+  },
   define: {
     global: "globalThis",
-  },
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
   },
 });
